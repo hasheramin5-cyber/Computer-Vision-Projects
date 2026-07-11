3ed6810727c91c18710ebed9b69a2a5c4391db94
Hasher Amin <hasheramin.5@gmail.com>
2026-07-11 12:35:40 +0500

    docs(assets): Adding Some Demo Pictures and Some in - Face Alignment.py

--- 02. Face Alignment/Face Alignment .py
+++ 02. Face Alignment/Face Alignment .py
@@ -1,11 +1,11 @@
 import cv2
 import mediapipe as mp
 
-# Initialize MediaPipe Face Mesh
+# MediaPipe Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1,refine_landmarks=True,min_detection_confidence=0.5,min_tracking_confidence=0.5)
 
-# Drawing utilities
+# Utilities
 mp_draw = mp.solutions.drawing_utils
 drawing_spec = mp_draw.DrawingSpec(thickness=1, circle_radius=1)
 
@@ -29,14 +29,12 @@ while True:
 
             # Draw all facial landmarks
             mp_draw.draw_landmarks(
-                image=frame,
-                landmark_list=face_landmarks,
+                image=frame, landmark_list=face_landmarks,
                 connections=mp_face_mesh.FACEMESH_TESSELATION,
                 landmark_drawing_spec=drawing_spec,
                 connection_drawing_spec=drawing_spec
             )
 
-            # -------- Face Alignment --------
 
             h, w, _ = frame.shape
 
